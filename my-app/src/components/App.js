498e0668189ba71f6e1bb982266fab08ed6f165b
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-31 23:51:13 +0300

    copy last changes

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Profile from './Profile/Profole';
 import user from './Profile/user.json';
+// import Statistic from './Statistic/Statistic';
+import statisticalData from './Statistic/statistic-data.json';
 
 const App = () => (
-  <Profile
-    name={user.name}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    {/* <Statistics title="Upload stats" stats={statisticalData} />; */}
+    {/* <Statistics stats={statisticalData} />; */}
+  </>
 );
 export default App;
