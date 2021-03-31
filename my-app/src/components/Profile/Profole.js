9d78d2e8a782414acf8c8e63352a997076e4e945
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-31 22:20:03 +0300

    add some changes

--- my-app/src/components/Profile/Profole.js
+++ my-app/src/components/Profile/Profole.js
@@ -1,36 +1,42 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./profile.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './profile.module.css';
 
-const Profile = () => (
+const Profile = ({ name, tag, location, avatar, stats }) => (
   <div class="profile">
     <div class="description">
-      <img
-        src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
-        alt="Аватар пользователя"
-        class="avatar"
-      />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+      <img src={avatar} alt="Аватар пользователя" class="avatar" />
+      <p class="name">{name}</p>
+      <p class="tag">@{tag}</p>
+      <p class="location">{location}</p>
     </div>
 
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+        <span class="quantity">{stats.followers}</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">2000</span>
+        <span class="quantity">{stats.views}</span>
       </li>
       <li>
         <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+        <span class="quantity">{stats.likes}</span>
       </li>
     </ul>
   </div>
 );
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
 export default Profile;
-/* <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />; */
-// };
