4a15d146688d5236b1600077bf5117d2786cbf24
Olena Abramova <olena.v.abramova@gmail.com>
2021-04-01 17:59:05 +0300

    create task 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Profile from './Profile/Profole';
+import Statistics from './Statistic/Statistic';
+
 import user from './Profile/user.json';
-// import Statistic from './Statistic/Statistic';
 import statisticalData from './Statistic/statistic-data.json';
 
 const App = () => (
@@ -13,8 +14,6 @@ const App = () => (
       avatar={user.avatar}
       stats={user.stats}
     />
-    {/* <Statistics title="Upload stats" stats={statisticalData} />; */}
-    {/* <Statistics stats={statisticalData} />; */}
   </>
 );
 export default App;
